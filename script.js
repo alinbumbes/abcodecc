26c364793301a76e3c125379688d62a9f2e37c9d
Alin Bumbes <alinbumbes@gmail.com>
2024-05-18 17:00:45 +0300

    counter works

--- script.js
+++ script.js
@@ -3,18 +3,48 @@ class Card extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     const template = document.getElementById("template");
-    
     const clone = template.content.cloneNode(true);
     this.shadowRoot.appendChild(clone);
-    this.heading= this.shadowRoot.querySelector('h3')
+    this.valueHeader = this.shadowRoot.querySelector("h3");
+    this.valueHeader.innerHTML = this.value;
+    this.substractBtn = this.shadowRoot.querySelector("#subtract");
+    this.addBtn = this.shadowRoot.querySelector("#add");
   }
 
-
-
-  get headingAttribute() {
-    return this.getAttribute("heading")
+  connectedCallback() {
+    this.addBtn.addEventListener("click", this.add.bind(this));
+    this.substractBtn.addEventListener("click", this.substract.bind(this));
   }
 
-  
+  get value() {
+    return this.getAttribute("value");
+  }
+
+  set value(val) {
+    this.setAttribute("value", val);
+  }
+
+  substract() {
+    this.value = +this.value - 1;
+    console.log(this.value);
+  }
+
+  add() {
+    this.value = +this.value + 1;
+    console.log(this.value);
+  }
+
+  static get observedAttributes() {
+    return ["value"];
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    this.valueHeader.innerHTML = this.value;
+  }
+
+  disconnectedCallback() {
+    this.substractBtn.addEventListener("click", this.substract);
+    this.addBtn.addEventListener("click", this.add);
+  }
 }
-customElements.define("ab-card", Card);
+customElements.define("lut-counter", Card);
